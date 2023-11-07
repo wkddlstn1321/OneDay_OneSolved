c106fb073f8a9ca5b0d962bd9d967843c45d6dbf
jang-insu <wkddlstn132@gm>
2023-11-07 23:51:58 +0900

    best앨범 hard

---
+++ 피로도/Main.java
@@ -0,0 +1,14 @@
+package 피로도;
+
+public class Main {
+	public static void main(String args[]) {
+		int[][] dungeons = {{80, 20}, {50, 40}, {30, 10}};
+		System.out.println(solution(80, dungeons));
+	}
+
+	public static int solution(int k, int[][] dungeons) {
+		int answer = -1;
+		
+		return answer;
+	}
+}
