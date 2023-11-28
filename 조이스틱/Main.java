9754998791808294dfea513d3263cd37816ce7be
jang-insu <wkddlstn132@gm>
2023-11-28 22:59:02 +0900

    조이스틱

--- 조이스틱/Main.java
+++ 조이스틱/Main.java
@@ -1,25 +1,31 @@
 package 조이스틱;
 
 import java.util.*;
+import java.awt.Point;
 
 public class Main {
+	public static int leftRightMove;
+
 	public static void main(String args[]) {
-		System.out.println(solution("BBBAAAABA"));
+		System.out.println(solution("BBBAAAAA"));
 	}
 
-	public static int getLeftRightMove(ArrayList<Integer> targetIdx, int size) {
-		int sum = 0;
-		int start = 0;
-		int end = 0;
-		int diff = 0;
-		for (int i = 0 ; i < targetIdx.size() - 1; i++) {
-			if (diff < targetIdx.get(i + 1) - targetIdx.get(i)) {
-				diff = targetIdx.get(i + 1) - targetIdx.get(i);
-				start = targetIdx.get(i) + 1;
-				end = targetIdx.get(i + 1) - 1;
-			}
-		}
-		return sum;
+	public static int getLeftMove(int curIdx, int destIdx, int listSize) {
+		int move = 0;
+		if (curIdx >= destIdx)
+			move = curIdx - destIdx;
+		else
+			move = curIdx + listSize - destIdx;
+		return move;
+	}
+
+	public static int getRightMove(int curIdx, int destIdx, int listSize) {
+		int move = 0;
+		if (curIdx <= destIdx)
+			move = destIdx - curIdx;
+		else
+			move = destIdx + listSize - curIdx;
+		return move;
 	}
 
 	public static int getUpDownMove(ArrayList<Integer> targetIdxList, String name) {
@@ -38,11 +44,43 @@ public class Main {
 	public static int solution(String name) {
 		int answer = 0;
 		int upDownMove = 0;
-		int leftRightMove = 0;
 		ArrayList<Integer> targetIdxList = new ArrayList<>();
 		upDownMove = getUpDownMove(targetIdxList, name);
-		leftRightMove = getLeftRightMove(targetIdxList, name.length() - 1);
-		System.out.println("move: " + leftRightMove);
+		leftRightMove = Integer.MAX_VALUE;
+		int listSize = targetIdxList.size();
+		int nameSize = name.length();
+		for (int start = 0; start < listSize; start++) {
+			int curIdx = 0;
+			int startFirst = 0;
+			int lastFirst = 0;
+			for (int i = 0; i < start; i++) {
+				startFirst += getRightMove(curIdx, targetIdxList.get(i), nameSize);
+				curIdx = targetIdxList.get(i);
+			}
+			for (int i = listSize - 1; i >= start; i--) {
+				startFirst += getLeftMove(curIdx, targetIdxList.get(i), nameSize);
+				curIdx = targetIdxList.get(i);
+			}
+			curIdx = 0;
+			for (int i = listSize - 1; i >= start; i--) {
+				lastFirst += getLeftMove(curIdx, targetIdxList.get(i), nameSize);
+				curIdx = targetIdxList.get(i);
+			}
+			for (int i = 0; i < start; i++) {
+				lastFirst += getRightMove(curIdx, targetIdxList.get(i), nameSize);
+				curIdx = targetIdxList.get(i);
+			}
+			leftRightMove = Math.min(leftRightMove, Math.min(startFirst, lastFirst));
+		}
+		int curIdx = 0;
+		int lastCase = 0;
+		for (int i = 0; i < listSize ; i++) {
+			lastCase += getRightMove(curIdx, targetIdxList.get(i), nameSize);
+			curIdx = targetIdxList.get(i);
+		}
+		leftRightMove = Math.min(leftRightMove, lastCase);
+		if (leftRightMove == Integer.MAX_VALUE)
+			leftRightMove = 0;
 		answer = upDownMove + leftRightMove;
 		return answer;
 	}
---
+++ 조이스틱/README.md
@@ -0,0 +1,9 @@
+### 조이스틱
+
+[문제링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860)
+
+# 해결과정
+
+이거 그리디 아닙니다!
+
+
