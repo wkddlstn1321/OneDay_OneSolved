25e7f3e2dd74f267bf31f2fe9e3952e07beb7739
jang-insu <wkddlstn132@gmail.com>
2024-02-07 05:49:38 +0900

    징검다리

--- 징검다리/Main.java
+++ 징검다리/Main.java
@@ -6,43 +6,36 @@ class Solution {
 	public int solution(int distance, int[] rocks, int n) {
 		int answer = 0;
 		ArrayList<Integer> sortRocks = new ArrayList<>();
-		ArrayList<Integer> disList = new ArrayList<>();
-		sortRocks.add(0);
 		sortRocks.add(distance);
 		for (int i : rocks) {
 			sortRocks.add(binarySearchMinIndex(sortRocks, i), i);
 		}
-		for (int i = 1; i < sortRocks.size(); i++) {
-			disList.add(sortRocks.get(i) - sortRocks.get(i - 1));
+		int rockLeng = sortRocks.size();
+		int left = 0;
+		int right = distance;
+		while (left <= right) {
+			int mid = (left + right) / 2;
+			int start = 0;
+			int cnt = 0;
+			for (int i = 0 ; i < rockLeng ; i++) {
+				if (sortRocks.get(i) - start < mid) {
+					cnt++;
+				} else {
+					start = sortRocks.get(i);
+				}
+			}
+			if (cnt > n) {
+				right = mid - 1;
+			} else {
+				answer = mid;
+				left = mid + 1;
+			}
 		}
-		System.out.println();
-		for (int i = 0; i < n; i++) {
-			int minIdx = findMinIndex(disList);
-			System.out.println("currValue: " + disList.get(minIdx));
-			int size = disList.size();
-			if (minIdx == 0) {
-				disList.set(1, disList.get(1) + disList.get(0));
-				disList.remove(minIdx);
-			} else if (minIdx == size - 1) {
-				disList.set(minIdx - 1, disList.get(minIdx - 1) + disList.get(minIdx));
-				disList.remove(minIdx);
-			} else { // (disList.get(minIdx) == disList.get(minIdx + 1)) {
-				disList.set(minIdx, disList.get(minIdx) + disList.get(minIdx + 1));
-				disList.remove(minIdx + 1);
-			} 
-			// else {
-			// 	disList.set(minIdx - 1, disList.get(minIdx - 1) + disList.get(minIdx));
-			// 	disList.remove(minIdx);
-			// }
-		}
-		for (int i : disList) {
-			System.out.println(i);
-		}
-		System.out.println();
-		answer = disList.get(findMinIndex(disList));
 		return answer;
 	}
 
+
+	//바위를 정렬할 때 사용하는 이진탐색 Arrays.sort를 사용해도 무방하다.
 	public int binarySearchMinIndex(ArrayList<Integer> list, int target) {
 		int left = 0;
 		int right = list.size() - 1;
@@ -55,13 +48,6 @@ class Solution {
 			}
 		}
 		return left;
-	}
-
-	public static int findMinIndex(ArrayList<Integer> list) {
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
-		pq.addAll(list);
-		int minIdx = list.indexOf(pq.poll());
-		return minIdx;
 	}
 }
 
