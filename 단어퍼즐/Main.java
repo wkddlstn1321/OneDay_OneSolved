9acae57bd5480b147a8c60a9332a9d68f61fce00
jang-insu <wkddlstn132@gmail.com>
2024-02-08 04:48:11 +0900

    퍼즐 조각 채우기 게임보드 테이블 퍼즐 파악까지

---
+++ 단어퍼즐/Main.java
@@ -0,0 +1,19 @@
+package 단어퍼즐;
+
+class Solution {
+	public int solution(String[] strs, String t) {
+		int answer = 0;
+		return answer;
+	}
+}
+
+// 단어 조지기
+// banan nana ba 
+public class Main {
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		String[] strs = {"ba","na","n","a"};
+		String t = "banana";
+		System.out.println(sol.solution(strs, t));
+	}
+}
---
+++ 퍼즐조각채우기/Main.java
@@ -0,0 +1,77 @@
+package 퍼즐조각채우기;
+
+// 게임보드 저장 식
+// 퍼즐조각 개수, 퍼즐 시작 좌표
+
+import java.util.*;
+import java.awt.Point;
+
+class Solution {
+	int[][] boardVisit;
+	int[][] tableVisit;
+	int[] dirX = { -1, 1, 0, 0 };
+	int[] dirY = { 0, 0, -1, 1 };
+	int[][] gGameBoard;
+	int[][] gTable;
+	int boardLength;
+
+	public int solution(int[][] game_board, int[][] table) {
+		int answer = -1;
+		boardLength = game_board.length;
+		gGameBoard = game_board;
+		gTable = table;
+		boardVisit = new int[boardLength][boardLength];
+		tableVisit = new int[boardLength][boardLength];
+		ArrayList<Integer> game_boardList = new ArrayList<>();
+		ArrayList<Integer> tableList = new ArrayList<>();
+		for (int i = 0; i < boardLength; i++) {
+			for (int j = 0; j < boardLength; j++) {
+				if (game_board[i][j] == 0 && boardVisit[i][j] == 0) {
+					game_boardList.add(getPuzzlePieceCnt(0, i, j));
+				}
+				if (table[i][j] == 1 && tableVisit[i][j] == 0) {
+					tableList.add(getPuzzlePieceCnt(1, i, j));
+				}
+			}
+		}
+		return answer;
+	}
+
+	private int getPuzzlePieceCnt(int state, int i, int j) {
+		int[][] board = state == 0 ? gGameBoard : gTable;
+		int[][] visit = state == 0 ? boardVisit : tableVisit;
+		Queue<Point> q = new LinkedList<>();
+		q.add(new Point(i, j));
+		visit[i][j] = 1;
+		int cnt = 1;
+		while (!q.isEmpty()) {
+			Point xy = q.poll();
+			for (int k = 0; k < 4; k++) {
+				int x = xy.y + dirX[k];
+				int y = xy.x + dirY[k];
+				if (0 <= x && x < boardLength && 0 <= y && y < boardLength) {
+					if (board[y][x] == state && visit[y][x] == 0) {
+						cnt++;
+						visit[y][x] = 1;
+						q.add(new Point(y, x));
+					}
+				}
+			}
+		}
+		return cnt;
+	}
+}
+
+// 퍼즐을 넣을 때 주변에 빈칸이 있으면 안되기 때문에 퍼즐을 맞추는 순서는 상관이 없다.
+// 모든 퍼즐을 대상으로 들어갈 곳이 있는지 확인만 하면 되는 것
+// 퍼즐을 끼워맞출 때 방향을 돌리면서 확인한다.
+public class Main {
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int[][] game_board = { { 1, 1, 0, 0, 1, 0 }, { 0, 0, 1, 0, 1, 0 }, { 0, 1, 1, 0, 0, 1 }, { 1, 1, 0, 1, 1, 1 },
+				{ 1, 0, 0, 0, 1, 0 }, { 0, 1, 1, 1, 0, 0 } };
+		int[][] table = { { 1, 0, 0, 1, 1, 0 }, { 1, 0, 1, 0, 1, 0 }, { 0, 1, 1, 0, 1, 1 }, { 0, 0, 1, 0, 0, 0 },
+				{ 1, 1, 0, 1, 1, 0 }, { 0, 1, 0, 0, 0, 0 } };
+		System.out.println(sol.solution(game_board, table));
+	}
+}
